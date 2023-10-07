254b10251df0a03ca9c1af1c7d30b10c327d5422
Sangeun Kim <dev.sangau20@gmail.com>
2023-10-07 16:54:51 +0900

    🎉 :: Initial Commit

---
+++ Dandle/DandleApp.swift
@@ -0,0 +1,17 @@
+//
+//  DandleApp.swift
+//  Dandle
+//
+//  Created by 김상금 on 2023/10/04.
+//
+
+import SwiftUI
+
+@main
+struct DandleApp: App {
+  var body: some Scene {
+    WindowGroup {
+      ContentView()
+    }
+  }
+}
